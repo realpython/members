6c4653616bb422376854e7b4bd6f0bdda0cc775e
Michael Herman <hermanmu@gmail.com>
2016-07-10 23:14:59 -0600

    updated tests

--- src/server/db/seeds/04_messages.js
+++ src/server/db/seeds/04_messages.js
@@ -1,24 +1,30 @@
 exports.seed = function(knex, Promise) {
-  return Promise.join(
-    // Deletes ALL existing entries
-    knex('messages').del(),
-    // Inserts seed entries
-    knex('messages').insert({
-      content: 'Awesome lesson!',
-      lesson_id: 1,
-      user_id: 1,
-      created_at: new Date(2015, 06, 9)
-    }),
-    knex('messages').insert({
-      content: 'Sick!',
-      lesson_id: 1,
-      user_id: 1,
-      created_at: new Date(2016, 06, 9)
-    }),
-    knex('messages').insert({
-      content: 'Love it!',
-      lesson_id: 2,
-      user_id: 1
-    })
-  );
+  return Promise.all([
+    // deletes ALL existing entries
+    knex('messages').del()
+  ]).then(function() {
+    return Promise.all([
+      knex('users').select('*')
+    ]);
+  }).then(function(users) {
+    return Promise.all([
+      knex('messages').insert({
+        content: 'Awesome lesson!',
+        lesson_id: 1,
+        user_id: 1,
+        created_at: new Date(2015, 06, 9)
+      }),
+      knex('messages').insert({
+        content: 'Sick!',
+        lesson_id: 1,
+        user_id: 1,
+        created_at: new Date(2016, 06, 9)
+      }),
+      knex('messages').insert({
+        content: 'Love it!',
+        lesson_id: 2,
+        user_id: 1
+      })
+    ]);
+  });
 };
