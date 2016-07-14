b722505a62e5a6b1dd12ca6ca103f2224bee3595
Michael Herman <hermanmu@gmail.com>
2016-07-13 20:10:41 -0600

    Users should be able to post new messages within the lesson view

--- src/server/db/seeds/03_users-messages.js
+++ src/server/db/seeds/03_users-messages.js
@@ -1,7 +1,18 @@
 exports.seed = function(knex, Promise) {
   return Promise.all([
     // deletes ALL existing entries
-    knex('messages').del()
+    knex('users').del(),
+    knex('messages').del(),
+    knex('users').insert({
+      github_username: 'Michael',
+      github_id: 987,
+      github_display_name: 'Michael Johnson',
+      github_access_token: '798',
+      github_avatar: 'https://avatars.io/static/default_128.jpg',
+      email: 'michael@johnson.com',
+      verified: false,
+      admin: false
+    })
   ]).then(function() {
     return Promise.all([
       knex('users').select('*')
