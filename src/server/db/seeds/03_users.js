3e9874fc69aa5703cdf59ccd31407fdc5a00ba9d
Michael Herman <hermanmu@gmail.com>
2016-08-07 10:22:55 -0600

    updated tests

--- src/server/db/seeds/03_users.js
+++ src/server/db/seeds/03_users.js
@@ -1,17 +1,20 @@
 exports.seed = function(knex, Promise) {
-  return Promise.join(
+  return Promise.all([
     // Deletes ALL existing entries
-    knex('users').del(),
+    knex('users').del()
+  ]).then(function() {
     // Inserts seed entries
-    knex('users').insert({
-      github_username: 'Michael',
-      github_id: 987,
-      github_display_name: 'Michael Johnson',
-      github_access_token: '798',
-      github_avatar: 'https://avatars.io/static/default_128.jpg',
-      email: 'michael@johnson.com',
-      verified: false,
-      admin: false
-    })
-  );
+    return Promise.all([
+      knex('users').insert({
+        github_username: 'Michael',
+        github_id: 987,
+        github_display_name: 'Michael Johnson',
+        github_access_token: '798',
+        github_avatar: 'https://avatars.io/static/default_128.jpg',
+        email: 'michael@johnson.com',
+        verified: false,
+        admin: false
+      })
+    ]);
+  });
 };
